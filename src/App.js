31f4a8bd2be0801e24f6584fde2501d64f12048c
Alex Entrepreneur <hello@alexentrepreneur.com>
2019-06-13 18:33:04 +0100

    .

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import { View } from './components/~reusables/atoms/View';
+import { Card } from './components/~reusables/atoms/Card';
+import { Container } from './components/~reusables/atoms/Container';
+import { H3 } from './components/~reusables/atoms/Text';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Design Systems with Styled Components 💅</h1>
-    </div>
+    <View>
+      <Card>
+        <Container>
+          <H3>Welcome Back!</H3>
+          <Text>To keep connected with us please login with your personal info.<Text>
+            <Button>Sign In</Button>
+        </Container>
+        <Container></Container>
+      </Card>
+    </View>
   );
 }
 
