fbcb494e9ddbded922848c93e763393287971154
Orie Chinedu <oriechinedu@gmail.com>
2019-06-13 20:08:04 +0100

    Make it possible to add padding props to accept both arrays and strings - Handles for when the padding props is not passed

--- src/components/~reusables/atoms/Container.js
+++ src/components/~reusables/atoms/Container.js
@@ -6,8 +6,7 @@ export const Container = styled.div`
   ${props => (props.vCenter ? `justify-content: center;` : null)}
   ${props => (props.column ? `flex-direction: column;` : null)}
   ${props => (props.hCenter ? `align-items: center;` : null)}
-  ${props => (Array.isArray(props.padding) ? `padding: ${props.padding[0]} ${props.padding[1]};` : null)}
-  ${props => (Array.isArray(props.padding) === false ? `padding: ${props.padding};` : null)}
+  ${props => (props.padding && Array.isArray(props.padding) ? `padding: ${props.padding.join(' ')};` : props.padding && !Array.isArray(props.padding) ? `padding: ${props.padding};` : '')}
   ${props => (props.width ? `width: ${props.width};` : null)}
   ${props => (props.bgColor ? `background-color: ${props.bgColor};` : null)}
 `;
