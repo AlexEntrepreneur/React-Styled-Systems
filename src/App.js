71bf6b6f2ae893692f10cee2958b682c4e2dd9e7
Alex Entrepreneur <hello@alexentrepreneur.com>
2019-06-13 18:42:29 +0100

    .

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import { View } from './components/~reusables/atoms/View';
 import { Card } from './components/~reusables/atoms/Card';
 import { Container } from './components/~reusables/atoms/Container';
 import { Form } from './components/~reusables/molecules/Form';
-import { Button } from './components/~reusables/atoms/Buttons';
+import { Button, ButtonPrimary } from './components/~reusables/atoms/Buttons';
 import { Input } from './components/~reusables/atoms/Inputs';
 import { Text, H3, SmallText } from './components/~reusables/atoms/Text';
+import { theme_secondary, theme_primary } from './components/~reusables/variables/colors';
+import { medium_space, extra_large_space, normal_space } from './components/~reusables/variables/spacing';
+import { desktop_width } from './components/~reusables/variables/media-queries';
 
 function App() {
   return (
-    <View hCenter vCenter>
+    <View hCenter vCenter bgColor={theme_secondary}>
       <Card>
         <Container>
           <H3>Welcome Back!</H3>
